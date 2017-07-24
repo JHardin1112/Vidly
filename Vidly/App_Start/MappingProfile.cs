3b1a72ec8ed068fd623d26e7913ad15af3086b7d
Jeremy Hardin <jeremyhardin@gmail.com>
2017-07-24 16:47:32 -0400

    checking in

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Models;
+using Vidly.DTOs;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Domain to Dto
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<MovieDto, Movie>();
+
+            //Dto to Domain
+            Mapper.CreateMap<Customer, CustomerDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<Movie, MovieDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+        }
+    }
+}
